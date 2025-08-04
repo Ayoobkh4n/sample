d452ab3ef199729fd56ed1551e0e4a5192af9ec9
Ayoobkh4n <khanayoob80@gmail.com>
2025-08-04 23:16:24 +0530

    01 inception

---
+++ app.js
@@ -0,0 +1,18 @@
+// const heading=React.createElement(
+//     "h1",
+//     {id:"heading"},
+//     "Hello World from React");
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", { id: "heading1" }, "This is H1 tag"),
+    React.createElement("h2", { id: "heading2" }, "THis is H2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", { id: "heading1" }, "This is H1 tag"),
+    React.createElement("h2", { id: "heading2" }, "THis is H2 tag"),
+  ]),
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+console.log(parent);
